9fcd7d36d69373c7b72a62113eb9057cbcb08f81
De Xian <ahx.agent007@gmail.com>
2020-02-02 21:50:43 +0600

    getting started

--- RMS.py
+++ RMS.py
@@ -10,6 +10,46 @@ def index():
 def manager_dashboard():
    return render_template('manager.html')
 
+@app.route('/Manager/Sales')
+def manager_sales():
+   return render_template('manager_sales.html')
+
+@app.route('/Manager/Stock')
+def manager_stock():
+   return render_template('manager_stock.html')
+
+@app.route('/Manager/EmpAll')
+def manager_emp_all():
+   return render_template('manager_emp_all.html')
+
+@app.route('/Manager/EmpWork')
+def manager_emp_work():
+   return render_template('manager_emp_work.html')
+
+@app.route('/Manager/Message')
+def manager_msg():
+   return render_template('manager_msg.html')
+
+@app.route('/Manager/Add/Employee')
+def manager_add_emp():
+   return render_template('manager_add_emp.html')
+
+@app.route('/Manager/Add/Menu')
+def manager_add_menu():
+   return render_template('manager_add_menu.html')
+
+@app.route('/Manager/Add/Dish')
+def manager_add_dish():
+   return render_template('manager_add_dish.html')
+
+@app.route('/Manager/Add/Table')
+def manager_add_table():
+   return render_template('manager_add_table.html')
+
+@app.route('/Manager/Add/Offers')
+def manager_add_offers():
+   return render_template('manager_add_offers.html')
+
 app.debug = True
 app.run()
 app.run(debug = True)
